dd1c19ac72e0d27527197bdfb0239ad596feab3e
Ichisada Shioko <ichisadashioko@gmail.com>
2019-10-07 12:49:54 +0700

    add sample project to use CartPoleEnv

---
+++ Samples/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ebby.Gym.Envs.Classic;
+using NumSharp;
+
+namespace Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var env = new CartPoleEnv();
+
+            int numEpisodes = 10;
+
+            for (int i = 0; i < numEpisodes; i++)
+            {
+                var state = env.Reset();
+                var done = false;
+
+                while (!done)
+                {
+
+                    // var action = agent.Process(state);
+                    var action = env.ActionSpace.Sample();
+
+                    var step = env.Step(action);
+                    var nextState = step.Observation;
+                    var reward = step.Reward;
+                    var info = step.Information;
+
+                    Console.WriteLine($"action: {action}, reward: {reward}, info: {info}, nextState: {nextState}");
+                    done = step.Done;
+
+                    state = nextState;
+                    env.Render();
+                }
+            }
+        }
+    }
+}
